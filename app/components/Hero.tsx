9936481373bf3000c11bd7bb4152b14f5f1d29c9
Md Christien <mdchristien12@gmail.com>
2024-06-24 18:53:36 +0100

    Build The Hero Section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,11 +1,54 @@
-import React from 'react'
+"use client";
+
+import { Cursor, Typewriter, useTypewriter } from "react-simple-typewriter";
+import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import Link from "next/link";
 
 const Hero = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [text, count] = useTypewriter({
+    words: ["Hi, I'm Mesueh Christien", "A Full Stack Developer"],
+    loop: true,
+    delaySpeed: 2000,
+  });
 
-export default Hero
+  return (
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+      <BackgroundCircles />
+      <div className="relative h-36 w-36">
+        <Image
+          alt="my profile picture"
+          src="/mdprofile.jpg"
+          fill
+          className="object-cover rounded-full mx-auto"
+        />
+      </div>
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Software Engineer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+
+        <div className="pt-3">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 
 export default function Home() {
-  return <main>
+  return <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
     {/* Header */}
     <Header />
 
     {/* Hero */}
-    <section id="hero" >
+    <section id="hero" className="snap-center">
        <Hero />
     </section>
 
